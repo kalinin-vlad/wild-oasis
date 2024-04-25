2e83f59deffe233eaa4abcedb734e23add845438
Kalinin Vladislav <Kalinavl1995@gmail.com>
2024-04-25 16:08:33 +0300

    feture(CreateCabinForm) add react-hook-form

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Settings from "./pages/Settings";
 import Account from "./pages/Account";
 import Users from "./pages/Users";
 import AppLayout from "./ui/AppLayout";
+import { Toaster } from "react-hot-toast";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -40,6 +41,29 @@ function App() {
         </Routes>
       </BrowserRouter>
       <GlobalStyles />
+      <Toaster
+        position="top-center"
+        gutter={12}
+        containerStyle={{
+          margin: "8px",
+        }}
+        toastOptions={{
+          success: {
+            duration: 3000,
+          },
+
+          error: {
+            duration: 5000,
+          },
+          style: {
+            fontSize: "16px",
+            maxWidth: "500px",
+            padding: "16px 24px",
+            backgroundColor: "var(--color-grey-0)",
+            color: "var(--color-grey-700)",
+          },
+        }}
+      />
     </QueryClientProvider>
   );
 }
