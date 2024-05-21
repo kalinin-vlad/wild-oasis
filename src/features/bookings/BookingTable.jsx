38f1615aecbdf1da425d3d65bb7418466a4b276c
Kalinin Vladislav <Kalinavl1995@gmail.com>
2024-05-22 00:07:19 +0300

    feature(main.jsx, ErrorFallback.jsx) add error boundaries

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -1,15 +1,16 @@
+import { useBookings } from "./useBookings";
+import { Pagination } from "../../ui/Pagination";
 import BookingRow from "./BookingRow";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
 import Empty from "../../ui/Empty";
-import { useBookings } from "./useBookings";
 import Spinner from "../../ui/Spinner";
-import { Pagination } from "../../ui/Pagination";
 
 function BookingTable() {
   const { bookings, isLoading, count } = useBookings();
 
   if (isLoading) return <Spinner />;
+
   if (!bookings.length) return <Empty resourceName="bookings" />;
 
   return (
